e2582b987a3193178e945823c732a35df9e8f00f
huyawen <18070034746@163.com>
2019-06-24 16:49:08 +0800

    test

--- lishidai/urls.py
+++ lishidai/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from  django.conf.urls import url
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apps/',include('apps.urls')),
