bd555c1fea15f664a71f8c05c75d7121d0723464
huyawen <18070034746@163.com>
2019-06-26 23:02:07 +0800

    19.06.26

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Login(models.Model):
+    ''''''
+
+    id=models.IntegerField(primary_key=True,unique=True)
+    phone=models.CharField(max_length=11)
+    pwd=models.CharField(max_length=18)
+    createtime=models.DateTimeField(auto_now_add=True)
+    class Meta():
+        db_table='login'
+
+    def __str__(self):
+        return self.phone
--- apps/views/login.py
+++ apps/views/login.py
@@ -1,6 +1,7 @@
 from   django.shortcuts  import  redirect,render_to_response,render,HttpResponse
 import  json
 from  apps.lib import fromInvalid
+from apps import models
 
 def  index(req):
     return render_to_response('login.html')
@@ -17,24 +18,25 @@ def  login(req):
         if  isInvalid:
             print('验证成功',obj.cleaned_data)
             print(obj.clean())
+            print('phone',req.POST.get('phone'))
+            obj=models.Login.objects.filter(phone=req.POST.get('phone')).values('pwd')
+            if req.POST.get('pwd')==obj[0]["pwd"]:
+                a={'msg':'登录成功','statu':200,'errorMassage':'null'}
+            else:
+                data['errorMassage']='账号或密码错误'
+                a = json.dumps(data)
 
-            a={'msg':'登录成功','statu':200,'errorMassage':'null'}
 
-            return HttpResponse(json.dumps(a))
+            return HttpResponse(a)
         else:
             print(obj.cleaned_data)
             print(obj.clean())
             print("错误信息",obj.errors)
-
-            try:
-                errorMassagePhone=obj.errors['phone']
-                data['errorMassagePhone']=obj.errors['phone']
-            except:
-                errorMassagePwd = obj.errors['pwd']
-                a1=print(obj.errors['pwd'])
-                print(errorMassagePwd)
-
-                a={'errorMassage':errorMassagePwd}
-                a=json.dumps(a)
-                return HttpResponse(a)
+            for  a  in  obj.errors:
+                try:
+                    data['errorMassage']=obj.errors[a][0]
+                except:
+                    pass
+            a=json.dumps(data)
+            return HttpResponse(a)
 
