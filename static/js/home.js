acdc48e3eaeebc69832d8a84093a9428b63774db
Von-fan <930690755@qq.com>
2019-06-30 00:47:52 +0800

    代码同步

---
+++ static/js/home.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Administrator on 2019/6/29.
+ */
+
+    //判断字符串是否属于列表
+        function inArray(a,b) {
+        for (var  i  in  b){
+            if(a==b[i]){
+                return true
+            }
+        }
+    }
+
+
+    //给二级dd标签添加display=none 隐藏起来
+    $('.left-one dl dd').addClass("active");
+
+    $('.left-one dl dt').addClass('icon iconfont icon-menu-fill');
+    $('.left-one dl dt span').addClass('icon iconfont icon-arrowBottom-fill');
+
+    $('.left-one dt').click(function () {
+        var cls=$(this).children('span').attr('class');
+        console.log(cls)
+        if (cls=="icon iconfont icon-arrowBottom-fill"){
+            $(this).children('span').attr('class','icon iconfont icon-arrowTop-fill');
+             $(this).siblings().removeClass("active")
+
+        }else {
+              $(this).children('span').attr('class','icon iconfont icon-arrowBottom-fill');
+              $(this).siblings().addClass("active")
+        }
+    })
+
+
+
+    //打开关闭左侧导航栏
+
+    $('.openLeft').click(function () {
+        var ElementOpenLeft=$(this).attr("class");
+        var ElementList=ElementOpenLeft.split(" ");
+        if (inArray("icon-icon-test5",ElementList)){
+            var intCode=ElementList.indexOf("icon-icon-test5");
+            ElementList[intCode]="icon-icon-test4";
+            strElement=ElementList.join(" ");
+            $(this).attr("class",strElement);
+
+            $(".left").addClass("active");
+            $('.right').css("margin-left","20px")
+
+        }else {
+            var intCode=ElementList.indexOf("icon-icon-test4");
+            ElementList[intCode]="icon-icon-test5";
+            strElement=ElementList.join(" ");
+            $(this).attr("class",strElement);
+            $(".left").removeClass("active");
+             $('.right').css("margin-left","200px")
+        }
+    })
+
+    //实时监控input方法
+
+    function acceptInp(element) {
+        $(element).on('input',function () {
+            console.log($(element).val());
+            var textInp=$(element).val();
+        if(textInp==""){
+            $(element).siblings().addClass("active")
+
+        }else {
+             $(element).siblings().removeClass("active")
+        }
+    })}
+
+    //一键删除输入框内容
+    function removeInp(removeEle,inpEle) {
+        $(removeEle).click(function () {
+            $(inpEle).val("");
+        })
+    }
+
+
+
+
+
