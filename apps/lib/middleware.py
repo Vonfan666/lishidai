f6dadf0991a21ba6977f9a7f20d2ac7a2c9080eb
huyawen <18070034746@163.com>
2019-07-19 18:53:33 +0800

    代码同步

--- apps/lib/middleware.py
+++ apps/lib/middleware.py
@@ -6,10 +6,29 @@ import  re
 class MyMiddleware(object):
     def __init__(self,request):
         self.request=request
-        self.noIncludedPath=["/login/",'/backLogin/']
+        self.noIncludedPath=["apps/login/",'apps/backLogin/']
         self.rePath=[re.compile(item)  for item in  self.noIncludedPath]
 
 
     def process_request(self,request):
         path=self.request.path
+        for item in  self.rePath:
+            if  item.match(path):
+                return render_to_response("login.html")
+        else:
+            if self.request.COOKIES.get("phone", None) == None:
+                return None
+            if self.request.session.get("phone", None) == None:
+                return None
+            if self.request.COOKIES.get("phone") == self.request.session["phone"] and self.request.COOKIES.get("pwd") == self.request.session["pwd"]:
+                return True
+    def process_view(self,request):
+        pass
+
+    def process_exception(self,request):
+        pass
+
+    def process_response(self,request):
+        pass
+
 
--- lishidai/settings.py
+++ lishidai/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'MyMiddleware',
 ]
 
 ROOT_URLCONF = 'lishidai.urls'
